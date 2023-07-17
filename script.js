d60be5f06cef7b7126e32ab3e6c47d8f2c649569
cursebreakers <esgvtt@gmail.com>
2023-07-17 15:35:44 -0400

    computer choices working in console

---
+++ script.js
@@ -0,0 +1,19 @@
+
+//Establish Game Parameters
+let choices = ["rock", "paper", "scissors"]; 
+
+//computer randomly selects one of 3x choices
+let computerInput = choices[Math.floor(Math.random()*3)];
+console.log("Computer chose" + ` ${computerInput}`);
+
+
+
+//player is prompted for choice
+
+
+
+//Player selects 
+    //rock, paper, or scissors
+
+//Winner is declared
+
