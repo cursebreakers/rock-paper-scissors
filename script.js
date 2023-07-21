48d6d80594c0d7bd3bc288a35530302277fc6eff
cursebreakers <esgvtt@gmail.com>
2023-07-21 14:19:40 -0400

    ui added

--- script.js
+++ script.js
@@ -1,53 +1,72 @@
-//Page loads,
-//Computer selection engine
+//Page loads game
+let userScore = 0;
+let compScore = 0;
+let milkWins = 0;
+let gameRound = 1;
+
+//Computer Selection Engine:
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const rng = Math.floor(Math.random() * 3);
     return choices[rng];
 }
-//Game is set up
-function round(userIn, computerChoice) {
-userIn = userIn.toLowerCase();
 
-if (userIn === computerChoice) {
-    return "Round is a tie with double " + userIn;
-    } else if (
-    (userIn === 'rock' && computerChoice === 'scissors') ||
-    (userIn === 'paper' && computerChoice === 'rock') ||
-    (userIn === 'scissors' && computerChoice === 'paper') ||
-    (userIn === 'milk' && computerChoice === 'paper')
-    ) {
-    return "player wins with " + userIn;
+//Round Play Engine:
+function round(userIn) {
+const computerChoice = getComputerChoice();
+let resultMessage;
+
+if (userIn === 'milk') {
+  if (computerChoice === 'paper') {
+    milkWins++;
+    userScore++;
+    resultMessage = 'Computer chose ' + computerChoice + '. Player wins with ' + userIn;
     } else {
-    return "player loses with " + userIn;
+    compScore++
+    resultMessage = 'Computer chose ' + computerChoice + '. Player loses with ' + userIn
+    }  
+  } else if (
+      (userIn === 'rock' && computerChoice === 'scissors') ||
+      (userIn === 'paper' && computerChoice === 'rock') ||
+      (userIn === 'scissors' && computerChoice === 'paper')
+    ) {
+      userScore++;
+      resultMessage = 'Computer chose ' + computerChoice + '. Player wins with ' + userIn;
+    } else if (userIn === computerChoice) {
+        resultMessage = 'Round is a tie with double ' + userIn;
+      } else {
+      compScore++;
+      resultMessage = 'Computer chose ' + computerChoice + '. Player loses with ' + userIn;
     }
-}
-//Function to start game 
-function game() {
-    let userScore = 0;
-    let compScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        //Player is prompted and makes selection
-        const userIn = prompt("Make a selection...");
-        const computerChoice = getComputerChoice();
-        console.log(round(userIn, computerChoice))
-        //push score
-        if (round(userIn, computerChoice).includes("wins")) {
-        userScore++;
-        } else if (round(userIn, computerChoice).includes("loses")) {
-        compScore++;
-        } 
-    }
-//Determine final score
-    if (userScore > compScore) {
-        console.log("player wins match");
-        } else if (userScore < compScore) {
-        console.log("computer wins match");
+    const resElm = document.getElementById('gamestat');
+    resElm.textContent = 'Round: ' + gameRound + ' - Player: ' + userScore + ' - Computer: ' + compScore;
+
+    const milkElm = document.getElementById('milkstat');
+    milkElm.textContent = 'Decisive Milk Victories: ' + milkWins;
+
+    const matchElm = document.getElementById('matchstat');
+    matchElm.textContent = resultMessage;
+
+    const endElm = document.getElementById('results');
+    
+    
+    if (gameRound === 5 || 10 || 15 || 20) {
+        let winner;
+        if (userScore > compScore) {
+            winner = 'Player ahead';
+          } else if (userScore < compScore) {
+            winner = 'Computer ahead';
+          } else {
+            winner = 'Tie';
+          }
+          endElm.textContent = winner;
         } else {
-        console.log("player ties match with computer");
-        }
+  }     gameRound++
 }
 
-//play
-game()
+//Reference to buttons & event listeners:
+document.getElementById('rock').addEventListener('click',  () => round('rock'));   
+document.getElementById('paper').addEventListener('click', () => round('paper'));  
+document.getElementById('scissors').addEventListener('click', () => round('scissors'));
+document.getElementById('milk').addEventListener('click', () => round('milk'));
